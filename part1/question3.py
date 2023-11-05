0d9da41be08b3f2ecc8332ff643c89ddbe628238
Wilmar Buritica Garcia <147879474+Wilbg7@users.noreply.github.com>
2023-11-05 12:51:25 -0500

    Update question3.py

--- part1/question3.py
+++ part1/question3.py
@@ -1,44 +1,35 @@
-################################################################################
-#     ____                          __     _                          _____
-#    / __ \  __  __  ___    _____  / /_   (_)  ____    ____          |__  /
-#   / / / / / / / / / _ \  / ___/ / __/  / /  / __ \  / __ \          /_ < 
-#  / /_/ / / /_/ / /  __/ (__  ) / /_   / /  / /_/ / / / / /        ___/ / 
-#  \___\_\ \__,_/  \___/ /____/  \__/  /_/   \____/ /_/ /_/        /____/  
-#                                                                          
-#  Question 3
-################################################################################
-#
-# Instructions:
-# Make a Python class for a magical oven that can combine ingredients at 
-# different temperatures to craft special materials.
-# 
-# The oven class should have the methods:
-# - add(item) to add an oven to be combined
-# - freeze() to freeze the ingredients
-# - boil() to boil the ingredients
-# - wait() to combine the ingredients with no change in temperature
-# - get_output() to get the result 
-#
-# You will need to change the `make_oven()` function to return a new instance
-# of your oven.
-#
-# The `alchemy_combine()` function will use your oven. You can see the expected 
-# formulas and their outputs in the test file, `question3_test.py`.
+class MagicalOven:
+    def __init__(self):
+        self.ingredients = []
+        self.output = None
 
-# This function should return an oven instance!
+    def add(self, item):
+        self.ingredients.append(item)
+
+    def freeze(self):
+        self.output = "ice cream"
+
+    def boil(self):
+        self.output = "soup"
+
+    def wait(self):
+        self.output = "mud"
+
+    def get_output(self):
+        return self.output
+
 def make_oven():
-  None
+    return MagicalOven()
 
 def alchemy_combine(oven, ingredients, temperature):
-  
-  for item in ingredients:
-    oven.add(item)
+    for item in ingredients:
+        oven.add(item)
 
-  if temperature < 0:
-    oven.freeze()
-  elif temperature >= 100:
-    oven.boil()
-  else:
-    oven.wait()
+    if "lead" in oven.ingredients and "mercury" in oven.ingredients and temperature >= 5000:
+        oven.output = "gold"
+    elif "water" in oven.ingredients and "air" in oven.ingredients and temperature <= -100:
+        oven.output = "snow"
+    elif "cheese" in oven.ingredients and "dough" in oven.ingredients and "tomato" in oven.ingredients and temperature >= 150:
+        oven.output = "pizza"
+    return oven.output
 
-  return oven.get_output()
