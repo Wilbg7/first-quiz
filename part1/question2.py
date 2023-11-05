214bac30debd1d195f0ff766a6b943f469db5deb
Wilmar Buritica Garcia <147879474+Wilbg7@users.noreply.github.com>
2023-11-05 12:50:33 -0500

    Update question2.py

--- part1/question2.py
+++ part1/question2.py
@@ -1,21 +1,9 @@
-################################################################################
-#     ____                          __     _                          ___ 
-#    / __ \  __  __  ___    _____  / /_   (_)  ____    ____          |__ \
-#   / / / / / / / / / _ \  / ___/ / __/  / /  / __ \  / __ \         __/ /
-#  / /_/ / / /_/ / /  __/ (__  ) / /_   / /  / /_/ / / / / /        / __/ 
-#  \___\_\ \__,_/  \___/ /____/  \__/  /_/   \____/ /_/ /_/        /____/ 
-#                                                                         
-#  Question 2
-################################################################################
-#
-# Instructions:
-# Write a function that will swap a tuple of two items. For example, the function 
-# should change (x, y) into (y, x). 
-# Assign the function to `swapper` so that the function `run_swapper(..)` can use
-# it. As always, there is a test suite that checks the result. It is in 
-# `question2_test.py.`
+def swapper(t):
+    x, y = t
+    return (y, x)
 
-swapper = None
+# Asigna la función `swapper` a la variable `swapper`
+swapper = swapper
 
 def run_swapper(list_of_tuples):
-  return list(map(swapper, list_of_tuples))
+    return list(map(swapper, list_of_tuples))
