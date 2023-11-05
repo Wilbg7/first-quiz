e2c4c6eceadd063d447701ee97470a0087d4a8f1
Wilmar Buritica Garcia <147879474+Wilbg7@users.noreply.github.com>
2023-11-05 12:34:18 -0500

    Update question1.py

--- part1/question1.py
+++ part1/question1.py
@@ -1,28 +1,17 @@
-################################################################################
-#     ____                          __     _                          ___
-#    / __ \  __  __  ___    _____  / /_   (_)  ____    ____          <  /
-#   / / / / / / / / / _ \  / ___/ / __/  / /  / __ \  / __ \         / / 
-#  / /_/ / / /_/ / /  __/ (__  ) / /_   / /  / /_/ / / / / /        / /  
-#  \___\_\ \__,_/  \___/ /____/  \__/  /_/   \____/ /_/ /_/        /_/   
-#                                                                        
-#  Question 1
-################################################################################
-#
-# Instructions:
-# The two functions below are used to tell the weather. They have some bugs that
-# need to be fixed. The test suite in `question1_test.py` will verify the output.
-# Read the test suite to know the values that these functions should return.
-
 def get_city_temperature(city):
    if city == "Quito":
-      return 22
+      return "22 degrees and sunny"
    if city == "Sao Paulo":
-      return 17
+      return "17 degrees and cloudy"
    if city == "San Francisco":
-      return 16
+      return "16 degrees and sunny"
+   if city == "New York":
+      return "14 degrees"
+   else:
+      return "City not found"
+
 
 def get_city_weather(city):
-
   sky_condition = None
 
   if city == "Sao Paulo":
@@ -32,4 +21,7 @@ def get_city_weather(city):
 
   temperature = get_city_temperature(city)
 
-  return str(temperature) + " degrees and " + sky_condition
+  if sky_condition is None:
+     return temperature
+  else:
+     return temperature + " and " + sky_condition
