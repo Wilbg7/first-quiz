5f7aed040a7f693f2f34edbabb97b703b352b3d5
Wilmar Buritica Garcia <147879474+Wilbg7@users.noreply.github.com>
2023-11-05 13:15:10 -0500

    Update VendingMachineImpl.java

--- part2/src/main/java/org/velezreyes/quiz/question6/VendingMachineImpl.java
+++ part2/src/main/java/org/velezreyes/quiz/question6/VendingMachineImpl.java
@@ -1,9 +1,12 @@
 package org.velezreyes.quiz.question6;
 
 public class VendingMachineImpl {
+    private static VendingMachine instance;
 
-  public static VendingMachine getInstance() {
-    // Fix me!
-    return null;
-  }
+    public static VendingMachine getInstance() {
+        if (instance == null) {
+            instance = new MyVendingMachine();
+        }
+        return instance;
+    }
 }
